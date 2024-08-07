e69850478ae5129659f25d11cda4b07eca253525
Amir Mehrabian <Amir.mehrabian1993@gmail.com>
2024-08-07 12:35:11 +0330

    [fix] Suggested revisions by Mojtaba added.

--- users/views.py
+++ users/views.py
@@ -7,13 +7,12 @@ from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
 
 from rest_framework import viewsets, status, permissions
-from rest_framework.exceptions import ValidationError
+
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from core import settings
 from .serializers import CreateUserSerializer, OtpSerializerRequest, \
     OtpSerializerVerification, UserSerializer, UserProfileUpdateSerializer
 
@@ -23,10 +22,7 @@ class UserRegisterView(CreateAPIView):
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        try:
-            serializer.is_valid(raise_exception=True)
-        except ValidationError:
-            return Response(data={"error": "Validation Error"}, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
 
         user_class = get_user_model()
         user_class.objects.create_user(username=serializer.data.get('username'),
@@ -34,7 +30,6 @@ class UserRegisterView(CreateAPIView):
                                        email=serializer.data.get('email'))
 
         return Response(data={
-            "msg": "User successfully created",
             "username": serializer.data.get('username'),
             "email": serializer.data.get('email'),
         },
