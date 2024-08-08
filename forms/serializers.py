8a0ea78ef47b9c2ed43f6b79281f55264afbe85e
ahmadrv <ahmadrv@protonmail.com>
2024-08-09 00:16:36 +0330

    [feature]: Question viewset and serializer.

--- forms/serializers.py
+++ forms/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Form
+from .models import Form, Question
 
 
 class FormSerializer(serializers.ModelSerializer):
@@ -9,3 +9,9 @@ class FormSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'password': {'write_only': True}
         }
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Question
+        fields = '__all__'
--- forms/views.py
+++ forms/views.py
@@ -1,9 +1,9 @@
 from rest_framework import viewsets, permissions
-from .models import Form
-from .serializers import FormSerializer
+from .models import Form, Question
+from .serializers import FormSerializer, QuestionSerializer
 
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class IsOwner(permissions.BasePermission):
     """
     This class can be used to grant permission only to
     the owner of the model object in that model's `ModelViewSet`
@@ -14,10 +14,31 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         return obj.user == request.user
 
 
+class IsQuestionOwner(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.form.user == request.user
+
+
 class FormViewSet(viewsets.ModelViewSet):
     queryset = Form.objects.all()
     serializer_class = FormSerializer
-    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+    permission_classes = [permissions.IsAuthenticated, IsQuestionOwner]
+
+    def perform_create(self, serializer):
+        if self.request.data.get('type') == Question.TEXT:
+            pass
+        elif self.request.data.get('type') == Question.SELECT:
+            pass
+        elif self.request.data.get('type') == Question.CHECKBOX:
+            pass
+
+        serializer.save(data=self.request.data)
