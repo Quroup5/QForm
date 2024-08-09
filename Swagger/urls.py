fd8efff4b83618be7f439ba1cb6539817a61406a
aminmoghadasi <amin.moghadasi84@gmail.com>
2024-08-09 18:12:43 +0330

    minor Change in swagger url

--- Swagger/urls.py
+++ Swagger/urls.py
@@ -1,16 +1,16 @@
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from django.urls import path, re_path
+from django.urls import path
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Your API Title",
+        title="Qform",
         default_version='v1',
-        description="Test description",
+        description="This is the most powerful form creator in the world",
         terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@yourapi.local"),
-        license=openapi.License(name="BSD License"),
+        contact=openapi.Contact(email="modir@gmail.com"),
+        license=openapi.License(name="MIT License"),
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
