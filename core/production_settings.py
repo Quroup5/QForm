bd199b76c97c7fce7e771fa95031e29675b041bc
ahmadrv <ahmadrv@protonmail.com>
2024-08-12 19:16:49 +0330

    [fix]: allowed host added (this should be revert)

--- core/production_settings.py
+++ core/production_settings.py
@@ -1,7 +1,7 @@
 from .settings import *
 
 DEBUG = env.bool('DJANGO_DEBUG', default=True)
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['127.0.0.1'])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['*'])
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
