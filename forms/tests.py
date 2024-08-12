f9edb212bb467a300dc5d909629d7183d598e371
Amir Mehrabian <Amir.mehrabian1993@gmail.com>
2024-08-12 12:58:36 +0330

    [feature] Test for process added.

--- forms/tests.py
+++ forms/tests.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from model_bakery import baker
-from .models import Form, Category, Process
+from .models import Form, Category, Process, FormProcess
 
 
 class FormViewSetTests(APITestCase):
@@ -216,3 +216,92 @@ class CategoryViewSetTests(APITestCase):
         # Ensure the category is deleted
         response = self.client.get('/categories/')
         self.assertEqual(len([c for c in response.data if c['id'] == self.category1.id]), 0)
+
+
+class ProcessViewSetTest(APITestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='test_user',
+            password='test_password')
+        self.client.force_authenticate(user=self.user)
+
+        self.response = self.client.post(reverse("process-list"), data={"title": "test_title",
+                                                                        "name": "test_name",
+                                                                        "type": "linear"})
+
+    def test_process_creation(self):
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Process.objects.first().name, "test_name")
+        self.assertEqual(Process.objects.first().type, "linear")
+        self.assertEqual(Process.objects.first().title, "test_title")
+
+    def test_process_delete(self):
+        response = self.client.delete(reverse("process-detail"
+                                              , args=[Process.objects.first().id]))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertIsNone(Process.objects.first())
+
+    def test_process_update(self):
+        response = self.client.put(reverse("process-detail"
+                                           , args=[Process.objects.first().id]),
+                                   data={"title": "new_title",
+                                         "name": "new_name",
+                                         "type": "free"})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Process.objects.first().name, "new_name")
+        self.assertEqual(Process.objects.first().type, "free")
+        self.assertEqual(Process.objects.first().title, "new_title")
+
+    def test_process_detail(self):
+        response = self.client.get(reverse("process-detail"
+                                           , args=[Process.objects.first().id]))
+        response_data = response.json()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response_data.get("name"), "test_name")
+        self.assertEqual(response_data.get("type"), "linear")
+        self.assertEqual(response_data.get("title"), "test_title")
+
+
+class FormProcessViewSetTest(APITestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='test_user',
+            password='test_password')
+        self.client.force_authenticate(user=self.user)
+        self.form = Form(title="test_form", user=self.user)
+        self.form.save()
+        self.process = Process(**{"title": "test_title",
+                                  "name": "test_process",
+                                  "type": "linear"}, user=self.user)
+        self.process.save()
+
+        self.response = self.client.post(reverse("formprocess-list"), data={"form": 1,
+                                                                            "process": 1,
+                                                                            "order": 1})
+
+    def test_formprocess_creation(self):
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(FormProcess.objects.first().process.name, "test_process")
+        self.assertEqual(FormProcess.objects.first().form.title, "test_form")
+
+    def test_formprocess_delete(self):
+        response = self.client.delete(reverse("formprocess-detail"
+                                              , args=[FormProcess.objects.first().id]))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertIsNone(FormProcess.objects.first())
+
+    def test_formprocess_update(self):
+        response = self.client.put(reverse("formprocess-detail"
+                                           , args=[Process.objects.first().id]),
+                                   data={"form": 1,
+                                         "process": 1,
+                                         "order": 2})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(FormProcess.objects.first().order, 2)
+
+    def test_formprocess_detail(self):
+        response = self.client.get(reverse("formprocess-detail"
+                                           , args=[FormProcess.objects.first().id]))
+        response_data = response.json()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response_data.get("order"), 1)
